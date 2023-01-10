260fc9796c6f811cf17748501f1a1b87f20a78c9
Jon Turner <theparticleman@gmail.com>
2023-01-09 21:57:35 -0700

    initial version of backup executable

--- Backup/Program.cs
+++ Backup/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EnvBackupRestore.Domain;
+
+var time = new Time();
+var fileSystem = new FileSystem();
+var settings = Settings.Load();
+
+var command = new BackupCommand(settings, fileSystem, time);
+
+command.Execute();
--- Domain/Settings.cs
+++ Domain/Settings.cs
@@ -1,7 +1,20 @@
+using System.Text.Json;
+
 namespace EnvBackupRestore.Domain;
 
 public class Settings
 {
     public string BackupDirectory { get; set; }
     public List<string> FilesToBackUp { get; set; }
+
+    static readonly JsonSerializerOptions options = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    };
+
+    public static Settings Load()
+    {
+        var settingsPath = "config.json";
+        return JsonSerializer.Deserialize<Settings>(File.ReadAllText(settingsPath), options);
+    }
 }

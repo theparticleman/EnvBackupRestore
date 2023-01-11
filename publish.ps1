24958aeee52380f66403ee44ae2a8bc027d14dce
Jon Turner <theparticleman@gmail.com>
2023-01-10 17:48:10 -0700

    publishing seems to work

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,7 @@
 dotnet publish -p:PublishSingleFile=true -r win-x64 -c Release --self-contained true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true
-# dotnet publish -p:PublishSingleFile=true -r osx-x64 -c Release --self-contained true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true
-# dotnet publish -p:PublishSingleFile=true -r linux-x64 -c Release --self-contained true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true
+dotnet publish -p:PublishSingleFile=true -r osx.11.0-arm64 -c Release --self-contained true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true
+dotnet publish -p:PublishSingleFile=true -r linux-x64 -c Release --self-contained true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true
 
-Compress-Archive .\Backup\bin\Release\net6.0\win-x64\publish\backup.exe env-backup-restore_win-64.zip
-# Compress-Archive .\bin\Release\net6.0\osx-x64\publish\mega-man-quotes mega-man-quotes_osx-x64.zip
-# Compress-Archive .\bin\Release\net6.0\linux-x64\publish\mega-man-quotes mega-man-quotes_linux-x64.zip
+Compress-Archive -LiteralPath.\Backup\bin\Release\net6.0\win-x64\publish\backup.exe, .\Restore\bin\Release\net6.0\win-x64\publish\restore.exe, config.json env-backup-restore_win-64.zip
+Compress-Archive -LiteralPath .\Backup\bin\Release\net6.0\osx.11.0-arm64\publish\backup, .\Restore\bin\Release\net6.0\osx.11.0-arm64\publish\restore, config.json env-backup-restore_osx-11-0-arm64.zip
+Compress-Archive -LiteralPath .\Backup\bin\Release\net6.0\linux-x64\publish\backup, .\Restore\bin\Release\net6.0\linux-x64\publish\restore, config.json env-backup-restore_linux-x64.zip
